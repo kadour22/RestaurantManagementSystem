c813712f68f7efa501424f198a6c0f536132a1d5
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-05 13:47:30 +0100

    add search and filters for restaurants

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .serializers import restaurant_serializer
 from .models import Restaurant
 
 class restaurant_viewset(ModelViewSet) :
     queryset = Restaurant.objects.all()
     serializer_class = restaurant_serializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name","adresse"]
