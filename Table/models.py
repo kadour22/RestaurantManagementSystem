3943ee16f907a9bac0b78aa45604f502ebe5626b
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-05 13:56:54 +0100

    create table models

--- Table/models.py
+++ Table/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Restaurant.models import Restaurant
+from uuid import uuid4
 
-# Create your models here.
+class Table(models.Model) :
+    table_number = models.CharField(max_length=2)
+    table_uuid = models.CharField(max_length=200, default=uuid4)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='table')
+
+    def __str__(self):
+        return f"{self.restaurant.name} {self.table_number}"
