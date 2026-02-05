02988a3d959b7d3f080c59f96471e3ddfee02540
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-05 13:31:40 +0100

    create restaurant CRUD

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import restaurant_serializer
+from .models import Restaurant
 
-# Create your views here.
+class restaurant_viewset(ModelViewSet) :
+    queryset = Restaurant.objects.all()
+    serializer_class = restaurant_serializer
