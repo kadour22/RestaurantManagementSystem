2f9c5b262b4fe85cd596a00d7faf0de475572f6b
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-05 13:23:14 +0100

    add Pillow to requirements file

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model) :
+    name = models.CharField(max_length=150, unique=True)
+    logo = models.ImageField(upload_to='logos')
+    adresse = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return self.name
+    
