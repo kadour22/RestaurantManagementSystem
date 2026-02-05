9628b2d23cb252384907446decaf6b4391fbb9b0
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-05 14:27:41 +0100

    add table routing

--- OrderManagmentSystem/urls.py
+++ OrderManagmentSystem/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Restaurant/', include('Restaurant.urls'))
+    path('Restaurant/', include('Restaurant.urls')),
+    path('Table/', include('Table.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
