8c20267a773c484d11246ed0c4be6bcb141639d9
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-05 14:40:55 +0100

    update

--- OrderManagmentSystem/urls.py
+++ OrderManagmentSystem/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('Restaurant/', include('Restaurant.urls')),
     path('Table/', include('Table.urls')),
+    path('Category/', include('Category.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
