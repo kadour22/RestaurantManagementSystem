2b9b32374def7fc94beae7274b9548b6141e0a13
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-05 13:44:03 +0100

    add media configurations

--- OrderManagmentSystem/settings.py
+++ OrderManagmentSystem/settings.py
@@ -89,5 +89,9 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
