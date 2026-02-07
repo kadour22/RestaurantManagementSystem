96b013bb79d02e52179fa67629c633bad7e7fe58
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-07 21:35:47 +0100

    update

--- Order/views.py
+++ Order/views.py
@@ -24,7 +24,3 @@ class order_service(APIView) :
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-        
-
