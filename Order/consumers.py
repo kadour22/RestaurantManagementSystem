d5e26a8e18811ba19b5c403c8fd7a400b83cbad9
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-08 14:07:24 +0100

    create ordering consumer

---
+++ Order/consumers.py
@@ -0,0 +1,26 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+
+class ordering_consumer(AsyncWebsocketConsumer) :
+
+    async def connect(self):
+        self.group_name = "orders"
+        await self.channel_layer.group_add(self.group_name,self.channel_name)
+        await self.accept()
+        print("connected")
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.group_name,self.channel_layer)
+        print("disconnected..")
+    
+    async def create_order(self,event) :
+        await self.send(
+            text_data=json.dumps({
+                "type":"create_order",
+                "table":event["table"],
+                "status":event["status"],
+                "total_price":event["total_price"],
+                "created_at":event["created_at"]
+            })
+        )
