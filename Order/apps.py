3e16b07a134c8ff206306dd285626a6ae3e0f481
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-11 22:04:17 +0100

    remove signals

--- Order/apps.py
+++ Order/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class OrderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Order'
-
-    # def ready(self):
-    #     import Order.signals
